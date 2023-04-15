9970de958fe33f079bd08e1de0b58393c60d7fa2
Adam <24621027+adoyle0@users.noreply.github.com>
2023-04-15 02:43:58 -0400

    too many words

--- lightning/src/routes/index.tsx
+++ lightning/src/routes/index.tsx
@@ -2,13 +2,13 @@ import { Title } from "solid-start";
 import BadassMap from "~/components/BadassMap";
 
 export default function Home() {
-  return (
-    <main>
-      <Title>Lightning</Title>
-      <h1>RIDE THE LIGHTNING</h1>
-      <BadassMap />
-      <p>Click and drag to watch me explode!
-      </p>
-    </main>
-  );
+    return (
+        <main>
+            <Title>Lightning</Title>
+            <h1>Lightning</h1>
+            <BadassMap />
+            <p>Click and drag to watch me explode!
+            </p>
+        </main>
+    );
 }
