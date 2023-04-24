4ff1febcaefa6503889a5964ee72d79c8e37bbb6
VenatioDecorus <opensourcekeys@gmail.com>
2023-04-24 02:13:44 -0400

    ehhh

--- lightning/src/lib/fetchStations.tsx
+++ lightning/src/lib/fetchStations.tsx
@@ -3,69 +3,73 @@ import { createSignal } from "solid-js";
 import { scatData, setScatData } from "~/root";
 
 type ChargingStation = {
-    Name: string
-    PhoneNumer: string
-    IntersectionDirections: string
-    AccessTime: string
-    Connectors: string[]
-    Network: string
-    Pricing: string
-    RestrictedAccess: boolean
-    CntLevel2Chargers: number
-    CntLevel3Chargers: number
+  Name: string;
+  PhoneNumer: string;
+  IntersectionDirections: string;
+  AccessTime: string;
+  Connectors: string[];
+  Network: string;
+  Pricing: string;
+  RestrictedAccess: boolean;
+  CntLevel2Chargers: number;
+  CntLevel3Chargers: number;
 };
 
 type Location = {
-    StreetAddress: string
-    City: string
-    State: string
-    Country: string
-    Zip: string
-    GeocodeStatus: string
-    Coordinates: string
-    CoordinateString: string
-    Stations: ChargingStation[]
+  StreetAddress: string;
+  City: string;
+  State: string;
+  Country: string;
+  Zip: string;
+  GeocodeStatus: string;
+  Coordinates: string;
+  CoordinateString: string;
+  Stations: ChargingStation[];
 };
 
-type StationRequest = {
-    Latitude: number
-    Longitude: number
-    Distance: number
-    CountLimit: number
+export type StationRequest = {
+  Latitude: number;
+  Longitude: number;
+  Distance: number;
+  CountLimit: number;
 };
 
-type StationResponse = {
-    Dist: number
-    Loc: Location
+export type StationResponse = {
+  Dist: number;
+  Loc: Location;
 };
 
 const TEST_PACKET: StationRequest = {
-    Latitude: 42.36,
-    Longitude: -71.05625,
-    Distance: 10,
-    CountLimit: 10,
+  Latitude: 42.36,
+  Longitude: -71.05625,
+  Distance: 10,
+  CountLimit: 10,
 };
 
-export const [stationsRequest, setStationsRequest] = createSignal<StationRequest>(TEST_PACKET)
+// export const [stationsRequest, setStationsRequest] = createSignal<StationRequest>(TEST_PACKET)
 
-export async function fetchStations() {
-    let response = await fetch('https://kevinfwu.com/getnearest', {
-        method: 'POST',
-        cache: 'default',
-        body: JSON.stringify(stationsRequest()),
-        headers: { 'Content-Type': 'application/json' }
+export async function fetchStations(
+  loc?: StationRequest
+): Promise<StationResponse[]> {
+  let response = await fetch("https://kevinfwu.com/getnearest", {
+    method: "POST",
+    cache: "default",
+    body: JSON.stringify(loc),
+    headers: { "Content-Type": "application/json" },
+  });
+  let resJson = await response.json();
+  console.log("Response JSON: ", resJson);
+  let buf = [];
+  for (let station of resJson) {
+    buf.push({
+      coordinates: [station.Loc.Coordinates[1], station.Loc.Coordinates[0]],
     });
-    let resJson = await response.json();
-    console.log('Response JSON: ', resJson);
-    let buf = [];
-    for (let station of resJson) {
-        buf.push({coordinates: [station.Loc.Coordinates[1], station.Loc.Coordinates[0]]})
-    };
-    console.log('Scat before: ', scatData());
-    console.log('Buffer: ', buf);
-    setScatData(buf);
-    console.log('Scat after: ', scatData());
+  }
+  console.log("Scat before: ", scatData());
+  console.log("Buffer: ", buf);
+  setScatData(buf);
+  console.log("Scat after: ", scatData());
 
-    //console.log(getCoords(resJson));
-    return resJson as StationResponse[];
-};
+  //console.log(getCoords(resJson));
+  return resJson as StationResponse[];
+}
